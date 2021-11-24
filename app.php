67e21c6be33d870936a04cdec19eabe6b427c040
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-11-24 00:08:22 -0600

    sidebar
    se maqueta el sidebar y navbar del aplicativo

---
+++ app.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inicio de Sistema | IscjlchavezG</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+    <!-- inicia navbar -->
+    <nav class="navbar shadow navbar-light bg-light fixed-top">
+    <div class="container-fluid mx-5">
+       <a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" class="text-decoration-none">
+        <svg class="bi" width="20" height="20" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#grid-fill"/>
+        </svg> <span class="text-dark"> Menu</span>
+      </a>
+      <ul class="navbar-nav my-lg-0 navbar-nav-scroll">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+
+
+
+    </div>
+  </nav>
+  <!-- termina navbar -->
+  <!-- inicia sidebar -->
+  <div class="offcanvas offcanvas-start d-flex flex-column flex-shrink-0 p-3 bg-light" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="width: 280px;">
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      <div class="container text-center">
+        <img src="img/user/yo.jpeg" alt="" width="50" height="50" class="rounded-circle me-4">
+        <span class="fs-4">Sistemas</span>
+      </div>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#speedometer2"/>
+        </svg>&nbsp; Dashboard</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#person-circle"/>
+        </svg>&nbsp; Perfil</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#people"/>
+        </svg>&nbsp; Usuarios</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#person-square"/>
+        </svg>&nbsp; Ofertantes</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#hammer"/>
+        </svg>&nbsp; Oficios</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#clipboard-check"/>
+        </svg>&nbsp; Solicitudes</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#clipboard-x"/>
+        </svg>&nbsp; Cancelaciones</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#credit-card-2-back-fill"/>
+        </svg>&nbsp; Tipo de Pago</a>
+      </li>
+      <li class="nav-item items">
+        <a href="#" class="nav-link fuente"><svg class="bi" width="23" height="23" fill="currentColor">
+           <use xlink:href="app/icons/bootstrap-icons.svg#calendar2-date"/>
+        </svg>&nbsp; Calendario</a>
+      </li>
+    </ul>
+  </div>
+  <!-- termina sidebar -->
+  <!-- inicia contenido -->
+  <div class="container pt-4">
+
+    <div class="mt-5">
+      texto
+    </div>
+
+  </div>
+  <!-- termina contenido -->
+  <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
