7d40ec0dc967ce4d5be457f412a67cf0c2a36cb8
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-11-25 09:26:41 -0600

    boton registrar
    se continua con la maquetta del registro de usuarios dentro del la app

--- Rusuario.php
+++ Rusuario.php
@@ -20,7 +20,10 @@
                                      <div class="row">
                                        <div class="col-sm-12 col-md-12 col-lg-4">
                                            <div class="py-2">
-                                               <span class="text-muted">Datos Generales</span>
+                                             <svg class="bi" width="15" height="15" fill="currentColor">
+                                                <use xlink:href="app/icons/bootstrap-icons.svg#person-lines-fill"/>
+                                             </svg>
+                                             <span class="text-muted">Datos Generales</span>
                                            </div>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="nom">
@@ -28,7 +31,7 @@
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#pencil"/>
                                                  </svg>
                                                </span>
-                                               <input type="text" class="form-control" name="nombre" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
+                                               <input type="text" class="form-control" name="nombre" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1" required>
                                            </div>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="apellidop">
@@ -36,7 +39,7 @@
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#pencil"/>
                                                  </svg>
                                                </span>
-                                               <input type="text" class="form-control" name="apellidop" placeholder="Apellido Paterno" aria-label="Paterno" aria-describedby="basic-addon1">
+                                               <input type="text" class="form-control" name="apellidop" placeholder="Apellido Paterno" aria-label="Paterno" aria-describedby="basic-addon1" required>
                                            </div>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="apellidom">
@@ -44,7 +47,7 @@
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#pencil"/>
                                                  </svg>
                                                </span>
-                                               <input type="text" class="form-control" name="apellidom" placeholder="Apellido Materno" aria-label="Materno" aria-describedby="basic-addon1">
+                                               <input type="text" class="form-control" name="apellidom" placeholder="Apellido Materno" aria-label="Materno" aria-describedby="basic-addon1" required>
                                            </div>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="telefono">
@@ -52,7 +55,7 @@
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#telephone"/>
                                                  </svg>
                                                </span>
-                                               <input type="tel" class="form-control" name="telefono" placeholder="Telefono" aria-label="Telefono" aria-describedby="basic-addon1">
+                                               <input type="tel" class="form-control" name="telefono" placeholder="Telefono" aria-label="Telefono" aria-describedby="basic-addon1" required>
                                            </div>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="email">
@@ -60,11 +63,11 @@
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#envelope"/>
                                                  </svg>
                                                </span>
-                                               <input type="email" class="form-control" name="email" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                                               <input type="email" class="form-control" name="email" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" required>
                                            </div>
                                            <div class="input-group mb-3">
                                                 <label class="input-group-text" for="inputGroupSelect01">Genero</label>
-                                                   <select class="form-select" id="inputGroupSelect01">
+                                                   <select class="form-select" id="genero" name="genero">
                                                       <option selected>Selecciona una opción</option>
                                                       <option value="1">Masculino</option>
                                                       <option value="2">Femenino</option>
@@ -74,20 +77,23 @@
                                        </div>
                                        <div class="col-sm-12 col-md-12 col-lg-4">
                                          <div class="py-2">
-                                             <span class="text-muted">Datos de servicio</span>
+                                           <svg class="bi" width="15" height="15" fill="currentColor">
+                                              <use xlink:href="app/icons/bootstrap-icons.svg#compass"/>
+                                           </svg>
+                                           <span class="text-muted">Datos de servicio</span>
                                          </div>
                                          <div class="input-group mb-3">
                                             <span class="input-group-text">Dirección</span>
-                                               <input type="text" name="calle" aria-label="First name" class="form-control" placeholder="Calle">
-                                               <input type="text" name="numero" aria-label="Last name" class="form-control" placeholder="Numero">
+                                               <input type="text" name="calle" aria-label="calle" class="form-control" placeholder="Calle" required>
+                                               <input type="text" name="numero" aria-label="numero" class="form-control" placeholder="Numero" required>
                                          </div>
                                          <div class="input-group mb-3">
                                             <span class="input-group-text" id="inputGroup-sizing-default">Direccion 2</span>
-                                            <input type="text" name="colonia" placeholder="Colonia" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                            <input type="text" name="colonia" placeholder="Colonia" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
                                          </div>
                                          <div class="input-group mb-3">
                                               <label class="input-group-text" for="inputGroupSelect01">Estado</label>
-                                                 <select class="form-select" id="inputGroupSelect01">
+                                                 <select class="form-select" id="estado" name="estado" required>
                                                     <option selected>Selecciona una opción</option>
                                                     <option value="1">Mexico</option>
                                                     <option value="2">Guadalajara</option>
@@ -96,22 +102,22 @@
                                           </div>
                                           <div class="input-group mb-3">
                                                <label class="input-group-text" for="inputGroupSelect01">Municipio</label>
-                                                  <select class="form-select" id="inputGroupSelect01">
+                                                  <select class="form-select" id="minicipio" name="municipio" required>
                                                      <option selected>Selecciona una opción</option>
                                                      <option value="1">Ciudad de Mexico</option>
                                                      <option value="2">Naucalpan</option>
                                                      <option value="3">Atizapan</option>
                                                   </select>
                                            </div>
-                                           <input type="text" name="latitud" id="latitud">
-                                           <input type="text" name="longitud" id="longitud">
+                                           <input type="text" name="latitud" id="latitud" required>
+                                           <input type="text" name="longitud" id="longitud" required>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="usuario">
                                                  <svg class="bi" width="15" height="15" fill="currentColor">
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#person-circle"/>
                                                  </svg>
                                                </span>
-                                               <input type="text" class="form-control" name="usuario" placeholder="Usuario" aria-label="Email" aria-describedby="basic-addon1">
+                                               <input type="text" class="form-control" name="usuario" placeholder="Usuario" aria-label="Email" aria-describedby="basic-addon1" required>
                                            </div>
                                            <div class="input-group mb-3">
                                                <span class="input-group-text" id="password">
@@ -119,13 +125,16 @@
                                                     <use xlink:href="app/icons/bootstrap-icons.svg#key-fill"/>
                                                  </svg>
                                                </span>
-                                               <input type="password" class="form-control" name="password" id="pass" placeholder="Password" aria-label="Email" aria-describedby="basic-addon1">
+                                               <input type="password" class="form-control" name="password" id="pass" placeholder="Password" aria-label="Email" aria-describedby="basic-addon1" required>
                                            </div>
                                        </div>
                                        <div class="col-sm-12 col-md-12 col-lg-4">
+                                         <div class="py-2">
+                                             <span class="text-muted">Seleccionar la direccion correcta</span>
+                                         </div>
                                           <div id="map"></div>
-                                          <div class="py-2">
-                                              <span class="text-muted">Seleccionar la direccion correcta</span>
+                                          <div class="py-1 d-grid gap-2">
+                                             <input type="submit" name="Registrar" value="Registrar" class="btn btn-sm btn-success" disabled>
                                           </div>
                                        </div>
                                      </div>
