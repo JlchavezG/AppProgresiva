bfd0c17b606ea85bb8f41ceb43396d892026ffb2
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-12-13 23:19:54 -0600

    actualizacion dasboar
    se actualiza dhasbiard

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,41 +1,64 @@
-<nav class="navbar shadow navbar-light bg-light navbar-expand-lg fixed-top">
-<div class="container-fluid mx-5">
-   <a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" class="text-decoration-none">
+
+<!-- inicia navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+  <a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" class="text-decoration-none">
     <svg class="bi" width="20" height="20" fill="currentColor">
        <use xlink:href="app/icons/bootstrap-icons.svg#grid-fill"/>
     </svg> <span class="text-dark"> Menu</span>
   </a>
-  <ul class="navbar-nav my-lg-0">
-    <li class="nav-item py-2">
-      <?php echo $usuario; ?>&nbsp;<img src="img/user/yo.jpeg" alt="perfil" style="width:30px; border-radius:50%;">
-    </li>
-    <li class="nav-item py-2">
-      &nbsp;&nbsp;
-      <a href="#" data-bs-toggle="modal" data-bs-target="#ModalCerrar"><svg class="bi" width="20" height="20" fill="currentColor">
-         <use xlink:href="app/icons/bootstrap-icons.svg#door-open-fill"/>
-      </svg></a>
-    </li>
-  </ul>
-</div>
-</nav>
-<!-- modal cerrar sesion -->
-<div class="modal fade" id="ModalCerrar" tabindex="-1" aria-labelledby="ModalCerrar" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="ModalCerrarLabel">Cerrar Sesion</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="text-center">
-          <!-- imagen del perfil -->
-        </div>
-        <span><?php echo $usuario; ?>&nbsp; ¿Deceas salir de tu sesión actual?</span>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger btn-sm" data-bs-dismiss="modal">Cancelar</button>
-        <a href="includes/CerrarS.php" class="btn btn-sm btn-success">Salir</a>
-      </div>
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarRightAlignExample" aria-controls="navbarRightAlignExample" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarRightAlignExample">
+      <!-- Left links -->
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <!-- Navbar dropdown -->
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-mdb-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </a>
+          <!-- Dropdown menu -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+              <a class="dropdown-item" href="#">Action</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Another action</a>
+            </li>
+            <li><hr class="dropdown-divider" /></li>
+            <li>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled"
+            >Disabled</a
+          >
+        </li>
+      </ul>
+      <!-- Left links -->
     </div>
+    <!-- Collapsible wrapper -->
   </div>
-</div>
+  <!-- Container wrapper -->
+</nav>
+<!-- termina navbar -->
