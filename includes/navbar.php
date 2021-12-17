1dc3ba643f8de18f126f9c4ece2061071d79eec2
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-12-17 08:01:38 -0600

    actualizacion navbar
    se trabaja en el disel¡ño de la ventana modal de cerrar sesion aun faltan procesos dentro del navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -23,14 +23,35 @@
        
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"> &nbsp; &nbsp; Link</a>
+          <a class="nav-link" href="#"> &nbsp; 
+            <svg class="bi" width="20" height="20" fill="currentColor">
+               <use xlink:href="app/icons/bootstrap-icons.svg#speedometer"/> 
+            </svg>&nbsp; Dahsboard
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"> &nbsp;
+            <svg class="bi" width="20" height="20" fill="currentColor">
+               <use xlink:href="app/icons/bootstrap-icons.svg#qr-code-scan"/> 
+            </svg>&nbsp; QR
+          </a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"> &nbsp;
+            <svg class="bi" width="20" height="20" fill="currentColor">
+               <use xlink:href="app/icons/bootstrap-icons.svg#plus-circle-fill"/> 
+            </svg>&nbsp; Nueva Solicitud
+          </a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Bienvenido: <?php echo $user['Nombre']; ?> <img src="img/user/<?php echo $user['Imagen']; ?>" alt="Imagen Perfil" style="width:30px; border-radius:50%;">
+            Hola: <?php echo $user['Nombre']; ?> &nbsp; <img src="img/user/<?php echo $user['Imagen']; ?>" alt="Imagen Perfil" style="width:30px; border-radius:50%;">
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Perfil</a></li>
+            <li><a class="dropdown-item" href="#">
+               <svg class="bi" width="20" height="20" fill="currentColor">
+                  <use xlink:href="app/icons/bootstrap-icons.svg#person-square"/> 
+               </svg> &nbsp; Perfil</a></li>
             <li class="dropdown-item">
                <div class="form-check form-switch">
                   <input class="form-check-input" type="checkbox" id="darkSwitch">
@@ -47,18 +68,45 @@
                </div>
             </li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+            <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#CSesion">
+            <svg class="bi" width="20" height="20" fill="currentColor">
+                  <use xlink:href="app/icons/bootstrap-icons.svg#power"/> 
+               </svg> &nbsp;Cerrar Sesión</a></li>
           </ul>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"> &nbsp;
+            <svg class="bi" width="20" height="20" fill="currentColor">
+               <use xlink:href="app/icons/bootstrap-icons.svg#bell-fill"/> 
+            </svg> <span class="badge bg-danger">4</span>
+          </a>
+        </li>
       </ul>  
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
-        <button class="btn btn-dark btn-sm" type="submit">
-            <svg class="bi" width="15" height="15" fill="currentColor">
-               <use xlink:href="app/icons/bootstrap-icons.svg#search"/> 
-            </svg> 
-        </button>
-      </form>
     </div>
   </div>
 </nav>
+<!-- termina navbar -->
+<!-- Modal Cerrar sesion-->
+<div class="modal fade" id="CSesion" tabindex="-1" aria-labelledby="CSesionLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered bg-ligth">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><?php echo $user['Nombre']; ?> ¿Deceas cerrar Sesión?</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="container text-center mb-2">
+           <img src="img/user/<?php echo $user['Imagen']; ?>" alt="perfil" style="width:90px; border-radius:50%;" >
+        </div>
+        <span class="text-center py-4"> ¿Estas seguro de queres salir de la plataforma y cerrar tu sesión?</span>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <a href="includes/CerrarS.php" class="btn btn-sm btn-danger">
+          <svg class="bi" width="20" height="20" fill="currentColor">
+               <use xlink:href="app/icons/bootstrap-icons.svg#power"/> 
+          </svg> Cerrar Sesión</a>
+      </div>
+    </div>
+  </div>
+</div>
