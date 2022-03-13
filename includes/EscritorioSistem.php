e9d4bcedcfe08ca4cc959a4e4ec07a8cac15554c
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2022-03-13 10:11:16 -0600

    se separa escritorio de sistemas
    el escritorio de sistemas se separo para crear consulta y colocar escritorio segun el usuario

---
+++ includes/EscritorioSistem.php
@@ -0,0 +1,192 @@
+<!-- inicia dasboard sistemas -->
+<div class="row text-center mt-5">
+         <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+             <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#person-circle"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Usuarios</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"> <?php echo $Tusuarios; ?></strong></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#briefcase-fill"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Ofertantes</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"> <?php echo $Tsocios; ?></strong></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#clipboard-check"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Solicitudes</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"> <?php echo $Tsolicitudes; ?></strong></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#clipboard-x"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Cancelación</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"> <?php echo $Tcancelaciones; ?></strong></div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div class="row text-center">
+         <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-6 col-md-6 col-lg-6 py-2">
+             <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#person-check-fill"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Usuarios Online</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"> <?php echo $TOnlineUsuarios; ?></strong></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-6 col-md-6 col-lg-6 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#person-bounding-box"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Tipo de Usuario</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-5"><?php echo $separar['AppTuser']; ?> </strong></div>
+                </div>
+             </div>
+          </div>
+       <!-- termina tarjeta -->
+       <!-- termina dasboard sistemas-->
+       <div class="mt-2">
+      <!-- inicia dasboard sistemas -->
+       <div class="row text-center">
+         <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+             <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#inboxes-fill"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center">Tickets</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"><?php echo $Ttickets; ?> </strong></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#hammer"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Oficios</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"><?php echo $Toficios; ?> </strong></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#calendar-day-fill"/>
+                    </svg>
+                    <div class="py-1">
+                    <? $fecha = date('d-m-Y');?>   
+                    <span class="fw-lighter me-2 text-center">Fecha: </span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <span class="fs-7"><?php echo $fecha; ?></span></div>
+                </div>
+             </div>
+          </div>
+          <!-- termina tarjeta -->
+          <!-- inicia tarjeta de dasboard -->
+          <div class="col-sm-3 col-md-3 col-lg-3 py-2">
+          <div class="card shadow bg-light container">
+                <div class="row">
+                <div class="col py-3">
+                    <svg class="bi" width="35" height="35" fill="currentColor">
+                       <use xlink:href="app/icons/bootstrap-icons.svg#alarm-fill"/>
+                    </svg>
+                    <div class="py-1">
+                    <span class="fw-lighter me-2 text-center"> Hora:</span>
+                    </div>
+                </div>
+                <div class="col py-4 ms-2">
+                  <strong class="fs-2"> </strong></div>
+                </div>
+             </div>
+          </div>
+       </div>
+
+
+
+       </div>
