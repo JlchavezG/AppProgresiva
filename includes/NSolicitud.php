c09ae9792014a5a28b32f4b84b0f0ab4d9604cdf
Jose Luis Chavez G <iscjlchavez@hotmail.com>
2022-07-18 12:00:01 -0500

    modificacion en solicitud

--- includes/NSolicitud.php
+++ includes/NSolicitud.php
@@ -1,28 +1,26 @@
 <?php
-$FechSolicitud = date('Y-m-d');
-$FechaFormat = $FechSolicitud = date('d-m-Y');
 $hora = date('H:i:s');
 
 ?>
 <div class="container mt-5">
- <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+ <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
    <div class="card shadow bg-light">
        <div class="row mt-2 container">
           <div class="col-sm-12 col-md-6 col-lg-6 py-1">
              <div class="input-group mb-1">
                 <span class="input-group-text" id="basic-addon1">Usuario solicitante</span>
-                <input type="text" class="form-control" id="Solicitante" placeholder="Solicitante" aria-label="Solicitante" aria-describedby="basic-addon1" value="<?php echo $separar['Nombre']." ".$separar['ApellidoP']; ?>" disabled>
+                <input type="text" class="form-control" name="Solicitante" id="Solicitante" placeholder="Solicitante" aria-label="Solicitante" aria-describedby="basic-addon1" value="<?php echo $separar['Nombre']." ".$separar['ApellidoP']; ?>" disabled>
              </div>
           </div>
           <div class="col-sm-12 col-md-6 col-lg-6 py-1">
             <div class="input-group mb-1">
-              <select class="form-select" id="inputGroupSelect02">
+              <select class="form-select" id="SolicitudOficio" name="SolicitudOficio" required>
                  <option selected>Selecciona un oficio...</option>
                  <?php while($RowOficios = $oficios->fetch_array()){?>
                  <option value="<?php echo $RowOficios['Id_Oficio']; ?>"><?php echo $RowOficios['NombreOf']; ?></option>
                  <?php } ?>
               </select>
-              <label class="input-group-text" for="inputGroupSelect02">Oficio Solicitado</label>
+              <label class="input-group-text" for="SolicitudOficio">Oficio Solicitado</label>
             </div>
           </div>
        </div>
@@ -30,7 +28,7 @@ $hora = date('H:i:s');
          <div class="col-sm-12 col-md-12 col-lg-12">
             <div class="input-group">
                <span class="input-group-text">Descripción del servicio:</span>
-               <textarea class="form-control" aria-label="With textarea"></textarea>
+               <textarea class="form-control" aria-label="With textarea" placeholder="Describe el servicio a realizar" required name="DescripcionServicio"></textarea>
             </div>
          </div>
        </div>
@@ -38,13 +36,13 @@ $hora = date('H:i:s');
          <div class="col-sm-12 col-md-6 col-lg-6">
              <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Fecha de la solicitud</span>
-                <input type="date" value="<?php echo $FechaFormat; ?>" class="form-control" required>
+                <input type="date" name="FechaSolicitud" value="<?php echo date('Y-m-d'); ?>" class="form-control" required disabled>
               </div>
          </div>
          <div class="col-sm-12 col-md-6 col-lg-6">
              <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Hora de la solicitud</span>
-                <input type="time" value="<?php echo $hora; ?>" class="form-control" required>
+                <input type="time" name="HoraSolicitud" value="<?php echo $hora; ?>" class="form-control" required disabled>
               </div>
          </div>
        </div>    
@@ -65,6 +63,29 @@ $hora = date('H:i:s');
                     <input type="time" name="HorarioSolicitado" class="form-control" required>
                   </div>
                </div>
+        </div>
+   </div>
+</div>
+<div class="container mt-3">
+   <div class="card shadow bg-light">
+        <div class="row mt-1 container">
+           <div class="col-sm-12 col-md-6 col-lg-6 mt-2">
+              <div class="input-group mb-3">
+                  <label class="input-group-text" for="estado">Estado</label> 
+                  <select class="form-select" id="estado" name="estado">
+                        <option selected> Selecciona una opción</option>
+                        <?php while($row1 = $estados->fetch_assoc()){ ?>
+                        <option value="<?php echo $row1['Id_Estado'];?>"><?php echo $row1['NombreE'];?></option> 
+                        <?php } ?>
+                  </select>
+               </div>
+           </div>
+           <div class="col-sm-12 col-md-6 col-lg-6 mt-2">
+               <div class="input-group mb-3">
+                  <label class="input-group-text" for="Municipio">Municipio</label> 
+                  <select class="form-select" id="municipio" name="municipio"></select>
+               </div>
+           </div>
         </div>
    </div>
 </div>
