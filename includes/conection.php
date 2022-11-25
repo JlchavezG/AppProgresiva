8a862ef5e0a24bbc48c00b39a1a795b296129cc3
Jose Luis Chavez G <iscjlchavez@hotmail.com>
2022-11-25 16:12:56 -0600

    Update conection.php

--- includes/conection.php
+++ includes/conection.php
@@ -1,8 +1,8 @@
 <?php
-$Host = "localhost";
-$UserHost = "root";
-$Database = "Hsoft";
-$Password = "";
+$Host = "localhost"; // servidor
+$UserHost = "root"; // usuario
+$Database = "Hsoft"; // base de datos 
+$Password = ""; // password 
 $conect = mysqli_connect($Host,$UserHost,$Password,$Database);
 if($conect->connect_errno){
   die('Error al conectar la base de datos del aplicativo'.$conecta->connect_error);
